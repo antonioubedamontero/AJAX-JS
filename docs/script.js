49d0c6e2cbe8a225f6f76109fc8de78a95500742
Antonio Ubeda Montero <anubedam@gmail.com>
2025-03-27 15:32:12 +0100

    fix api change error

--- docs/script.js
+++ docs/script.js
@@ -1,18 +1,18 @@
-// NOTA: No utilizamos "load" porque el código se ejecuta desde el HTML (renderizado)
+// NOTE: We do not use "load" because the code is executed from the HTML (rendered)
 
-function peticion(){
-    /*  1.- Creamos la petición */
-    var peticion = new XMLHttpRequest();
-    const URL = "https://restcountries.eu/rest/v2/all";
+function petition(){
+    /* 1.- We create the request */
+    const petitionCall = new XMLHttpRequest();
+    const URL = "https://restcountries.com/v3.1/all";
 
-    /* 2.- Ejecutamos código para cada cambio de estado:
+    /* 2.- We execute code for each state change:
      *   0: request not initialized
      *   1: server connection established
      *   2: request received
      *   3: processing request
      *   4: request finished and response is ready 
     */
-   peticion.onreadystatechange = function(){
+   petitionCall.onreadystatechange = function(){
        if (this.readyState == 4 && this.status == 200){    
           /* Algunos de los estados:
            * 200: "OK"
@@ -20,68 +20,78 @@ function peticion(){
            * 404: "Not Found"
           */
 
-        procesarRespuesta(JSON.parse(this.responseText)); // tb responseXML 
+        processResponse(JSON.parse(this.responseText)); // tb responseXML 
        }
 
-       /*if (this.readyState == 4){   
-         alert("Código de respuesta: "+this.status+" - mensaje: "+this.statusText);
-       }*/
+       /* 
+          if (this.readyState == 4){   
+            alert("Código de respuesta: "+this.status+" - mensaje: "+this.statusText);
+          } 
+        */
    };
 
-   /* 3.- Especificamos la peticion (true=llamada asíncrona; false=llamada síncrona) */
-   peticion.open("GET",URL,true); 
+   /* 3.- We specify the request (true=asynchronous call; false=synchronous call) */
+   petitionCall.open("GET",URL,true); 
    
-   /* 4.- Enviamos la petiicion */
-   peticion.send();
+   /* 4.- We send the request */
+   petitionCall.send();
 }
 
-/* 5.- Procesamos los datos de la respuesta */
-function procesarRespuesta(paises){
-    var tabla = document.getElementById("tabla");
+/* 5.- We process the response data */
+function processResponse(countries){
+    const htmlTable = document.getElementById("table");
   
-    for (pais of paises){
-        var fila = document.createElement("tr");
+    for (const country of countries){
+        const row = document.createElement("tr");
 
-        var tdPais = document.createElement("td");
-        var tdCapital = document.createElement("td");
-        var tdMoneda = document.createElement("td");
-        var tdIdioma = document.createElement("td");     
-        var tdBandera = document.createElement("td");      
+        const tdCountry = document.createElement("td");
+        const tdCapital = document.createElement("td");
+        const tdCurrency = document.createElement("td");
+        const tdLanguage = document.createElement("td");     
+        const tdFlag = document.createElement("td");      
 
-        tdPais.textContent = pais.name;
-        fila.appendChild(tdPais);
+        tdCountry.textContent = country.name.common;
+        row.appendChild(tdCountry);
 
-        tdCapital.textContent = pais.capital;
-        fila.appendChild(tdCapital);  
+        tdCapital.textContent = country.capital?.join(", ");
+        row.appendChild(tdCapital);  
 
-        /* Puede haber varias monedas */
-        var monedas = "<ul>";
-        for (moneda of pais.currencies){
-            monedas += `<li>${moneda.name}</li>`; 
+        /* There may be several currencies */
+        let currencies = "<ul>";
+
+        if (country.currencies) {
+            for (const currency of Object.keys(country.currencies)){
+                currencies += `<li>${country.currencies[currency].name}</li>`; 
+            }
         }
-        monedas +="</ul>"
-        tdMoneda.innerHTML = monedas;
-        fila.appendChild(tdMoneda);
 
-        /* Puede haber varios idiomas */
-        var idiomas = "<ul>";
-        for (idioma of pais.languages){
-            idiomas += `<li>${idioma.name}</li>`; 
+        currencies +="</ul>"
+        tdCurrency.innerHTML = currencies;
+        row.appendChild(tdCurrency);
+
+        /* There may be several languages */
+        let languages = "<ul>";
+
+        if (country.languages) {
+            for (const language of Object.keys(country.languages)){
+                languages += `<li>${country.languages[language]}</li>`; 
+            }
         }
-        idiomas +="</ul>"
-        tdIdioma.innerHTML = idiomas;
-        fila.appendChild(tdIdioma);
-
-        /* Bandera: incluiremos una imagen */
-        tdBandera.innerHTML = `<img src="${pais.flag}" height="100px" width="200px" alt="bandera">`;
-      
-        fila.appendChild(tdBandera);
         
-        tabla.appendChild(fila);
+        languages +="</ul>"
+        tdLanguage.innerHTML = languages;
+        row.appendChild(tdLanguage);
+
+        /* Flag: we will include an image */
+        tdFlag.innerHTML = `<img src="${country.flags.png}" height="100px" width="200px" alt="${country.flags.alt}">`;
+      
+        row.appendChild(tdFlag);
+
+        htmlTable.appendChild(row);
     }
 }
 
-function limpiar(){
-    var tabla = document.getElementById("tabla");
-    tabla.innerHTML = "";    
+function clearTable(){
+    const htmlTable = document.getElementById("table");
+    htmlTable.innerHTML = "";    
 }
